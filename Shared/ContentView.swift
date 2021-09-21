b0d5d345c83613136d45c925ad2cc91b21067a4f
Vitalson Lorran Viana Carvalhal <vitalson_lorran95@ymail.com>
2021-09-21 17:32:07 -0300

    Tentativa 2

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Vitalson Lorran on 21/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Tela_Login_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tela_Login_AppApp.swift
+//  Shared
+//
+//  Created by Vitalson Lorran on 21/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Tela_Login_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
